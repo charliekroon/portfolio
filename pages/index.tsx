b004071df7aa7c44a8c2e2a5e473f970f2180365
Charlie Kroon <charlotkroon@protonmail.com>
2022-11-06 10:58:02 +0100

    add head + todo

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,30 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
+//to do:
+// - replace <br />
+// - replace css by styledcomponents
+// - replace font
+// - add head
+
 export default function Home() {
+	const charlie = "Charlie Kroon";
+	const name = charlie.toUpperCase();
+	const description =
+		"Software Engineer with a background in Journalism";
+
 	return (
 		<>
+			<Head>
+				<title>Charlie Kroon</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>{" "}
 			<span className={styles.container}>
 				<span className={styles.description}>
-					{" "}
-					CHARLITA!
+					{name}
+					<br />
+					<br />
+					{description}
 				</span>
 				<span className={styles.content}>
 					<Link href="/about">
