e62161b036586083da5b7319777508cc830303fe
Charlie Kroon <charlotkroon@protonmail.com>
2022-11-06 22:16:13 +0100

    more

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,32 @@
 import MenuBar from "../components/MenuBar";
-import {AboutContainer, Title, Text} from "./background";
+import {AboutContainer} from "./background";
+import styled from "styled-components";
+
+export const Text = styled.text`
+	font-family: "Monteserrat";
+	font-weight: 300;
+	letter-spacing: 1px;
+	font-size: 15px;
+	max-width: 80%;
+	color: #1a1919;
+	display: grid;
+	margin-left: 10%;
+	flex-grow: 1;
+	margin-right: 40%;
+`;
+
+export const Title = styled.p`
+	font-family: "Sonder";
+	font-weight: 100;
+	font-style: light-weight;
+	color: #1a1919;
+	font-size: 150px;
+	letter-spacing: 3.5px;
+	margin-left: 10%;
+	flex-grow: 1;
+	margin-bottom: -30%;
+	margin-top: 10%;
+`;
 
 export default function Contact() {
 	return (
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,6 +1,21 @@
 import MenuBar from "../components/MenuBar";
-import {AboutContainer, Title, Text} from "./background";
+import {AboutContainer, Title} from "./background";
+import styled from "styled-components";
 
+export const Text = styled.text`
+	font-family: "Monteserrat";
+	font-weight: 300;
+	letter-spacing: 1px;
+	font-size: 15px;
+	max-width: 80%;
+	color: #1a1919;
+	line-height: 1.9em;
+	display: grid;
+	margin-left: 10%;
+	flex-grow: 1;
+	margin-top: -2%;
+	margin-right: 40%;
+`;
 export default function Work() {
 	return (
 		<>
