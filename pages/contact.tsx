0c5b97d52e56c1fb8b660d0705fdaab0d80c9d4a
Charlie Kroon <charlotkroon@protonmail.com>
2022-11-20 18:19:54 +0100

    add linkedin

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -50,6 +50,9 @@ export default function Contact() {
 						</Link>{" "}
 						<Link href="https://medium.com/@charliekroon">
 							<Image src="/medium.svg" height={30} width={30} alt="medium" />
+						</Link>{" "}
+						<Link href="https://www.linkedin.com/in/charliekroon/">
+							<Image src="/linkedin.svg" height={30} width={30} alt="medium" />
 						</Link>
 					</Icon>
 				</Text>
