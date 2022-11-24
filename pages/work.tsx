88c88b735df43d8bd6cca56fa1166b77187f9206
Charlie Kroon <charlotkroon@protonmail.com>
2022-11-24 09:43:02 +0100

    add Medium Api

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,17 +1,48 @@
 import MenuBar from "../components/MenuBar";
 import {General, StandardText, Title} from "../styles/Global";
 import {Headertje, ProjectsText} from "../styles/projects.styles";
+import React, {useState, useEffect} from "react";
+
 export default function Projects() {
-	return (
-		<>
-			<MenuBar />
-			<General>
-				<Title>Work</Title>
-				<Headertje>Work in Progress</Headertje>
-				<ProjectsText>
-					<StandardText></StandardText>
-				</ProjectsText>
-			</General>
-		</>
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [blogs, setBlogs] = useState<any[]>([]);
+	useEffect(() => {
+		fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@charliekroon/")
+			.then(res => res.json())
+			.then(
+				data => {
+					setIsLoaded(true);
+					setBlogs(data.items);
+				},
+				error => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	console.log("blogs", blogs);
+	console.log(
+		"blog map",
+		blogs.map(item => {
+			item;
+		})
 	);
+
+	if (error) {
+		return <div>Error: {error}</div>;
+	} else if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<>
+				<MenuBar />
+				<General>
+					<Title>Work</Title>
+					<>{blogs.map(item => item.title)}</>
+				</General>
+			</>
+		);
+	}
 }
