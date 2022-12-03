2de325417c712c12d34077d467468748b5352f4d
Charlie Kroon <charlotkroon@protonmail.com>
2022-12-03 19:58:00 +0100

    modifying work

--- pages/work.tsx
+++ pages/work.tsx
@@ -3,6 +3,7 @@ import {General, StandardText, Title, Text} from "../styles/Global";
 import React, {useState, useEffect} from "react";
 import {getBlogs} from "./api/blogAPI";
 import {Blog} from "../src/interfaces/blog";
+import Link from "next/link";
 
 export default function Projects() {
 	const [blogs, setBlogs] = useState<Blog[]>([]);
@@ -16,17 +17,22 @@ export default function Projects() {
 		return setBlogs(allBlogs);
 	};
 
-	const displayBlogjes = blogs.map(i => i.title);
+	const displayBlogjes = blogs.map(blog => (
+		<>
+			<Text>
+				<StandardText>
+					<Link href={blog.link}>{blog.title}</Link>
+				</StandardText>
+			</Text>
+		</>
+	));
 
 	return (
 		<>
 			<MenuBar />
 			<General>
 				<Title>Work</Title>
-				<Text>
-					{" "}
-					<StandardText>{displayBlogjes}</StandardText>
-				</Text>
+				{displayBlogjes}
 			</General>
 		</>
 	);
